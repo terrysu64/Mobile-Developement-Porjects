a0a930e2995c413727794f7335796aa9aa6a0877
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2022-01-19 20:01:43 -0500

    added start/pause ⏯

--- QuickFocus/components/countdown/countdown.js
+++ QuickFocus/components/countdown/countdown.js
@@ -21,9 +21,12 @@ export const Countdown = ({mins=20, isPaused}) => {
   };
   // useEffect is similar to componentDidMount and componentDidUpdate (it's like a condiiton for rendering)
   useEffect(() => {
+    if (isPaused) {
+      return
+    }
     interval.current = setInterval(()=>countDown(ms),1000)
     return () => clearInterval(interval.current)
-  },[]);
+  },[isPaused]);
 
   const minutes = Math.floor(ms/60000)
   const seconds = Math.floor((ms%60000)/1000)
--- QuickFocus/components/timer/timer.js
+++ QuickFocus/components/timer/timer.js
@@ -1,14 +1,23 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import {Countdown} from '../countdown/countdown'
- 
+import {Countdown} from '../countdown/countdown';
+//AddFocus will act as the starting button
+import {AddFocus} from '../add-focus/add-focus';
+
 export const Timer = ({currFocus}) => {
+
+  const [isStarted,setIsStarted] = useState(false);
 
   return (
     <View style={styles.container}>
-      <Countdown/>
+      <Countdown isPaused={!isStarted}/>
       <Text style={styles.title}>Currently Focusing On:</Text>
       <Text style={styles.task}>{currFocus}</Text>
+      <View style={styles.buttonWrapper}>
+        {isStarted ? 
+        (<AddFocus title={'pause'} onPress={()=>{setIsStarted(false)}}/>) : 
+        (<AddFocus title={'start'} onPress={()=>{setIsStarted(true)}}/>)}
+      </View>
     </View>
   );
   
@@ -30,6 +39,13 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
+  },
+
+  buttonWrapper: {
+    flex: 0.3,
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
 });
