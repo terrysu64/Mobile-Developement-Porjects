487c5b1b0ae6777d1c2f12e10b2166e92a44ec82
terrysu64 <66130506+terrysu64@users.noreply.github.com>
2022-02-23 19:57:44 -0500

    changed vicinity variable to address

--- GoodEats/src/services/restaurants/restaurants-service.js
+++ GoodEats/src/services/restaurants/restaurants-service.js
@@ -17,6 +17,7 @@ export const restaurantsTransform = ({ results = [] }) => {
         restaurant.photos = mockImages
         return {
             ... restaurant,
+            address: restaurant.vicinity,
             isOpen: restaurant.opening_hours && restaurant.opening_hours.open_now,
             isClosedTemporarily: restaurant.business_status !== "OPERATIONAL", 
         }
